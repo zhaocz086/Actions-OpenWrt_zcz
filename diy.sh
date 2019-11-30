05449f352c43fdee618d77ec73df678716233190
zhaocz086 <zhaochongzhong@gmail.com>
2019-11-30 23:23:10 +0800

    chang passwd

--- diy.sh
+++ diy.sh
@@ -8,4 +8,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # Modify default Password
-#sed -i 's/sadfasdfsad/asdgfdlas;kfas/g' package/lean/zzzz
+sed -i 's#\$1\$V4UetPzk\$CYXluq4wUazHjmCDBCqXF\.#\$1\$Ni2VoKC0\$\.3wJvdxTaV2Tgk\.5KXh7a\/#g' package/lean/default-settings/files/zzz-default-settings
